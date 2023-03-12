e5f31df4a863aab06a2ddc481c7507da9ed62763
Matias Volpe <kratico@users.noreply.github.com>
2023-03-12 11:18:45 -0300

    feat: add exception recipe

---
+++ cookbook/06_exceptions/IntArray.cpp
@@ -0,0 +1,46 @@
+#include "IntArray.h"
+#include <stdexcept>
+
+namespace cookbook {
+
+IntArray::IntArray() {
+  for (int i = 0; i < kSize; ++i) {
+    m_data[i] = 0;
+  }
+}
+
+int &IntArray::operator[](int index) {
+  if (index < 0 || index >= kSize) {
+    // throw custom exception
+    throw BadIndexError{index};
+    // throw custom exception + custom message
+    // throw BadIndexError{index, "invalid index"};
+    // throw standard exception
+    // throw std::out_of_range{"bad index"};
+  }
+
+  return m_data[index];
+}
+
+int IntArray::operator[](int index) const {
+  if (index < 0 || index >= kSize) {
+    // throw custom exception
+    throw BadIndexError{index};
+    // throw standard exception
+    // throw std::out_of_range{"bad index"};
+  }
+
+  return m_data[index];
+}
+
+std::ostream &operator<<(std::ostream &os, const IntArray &int_list) {
+  std::cout << "[";
+  for (int i = 0; i < int_list.kSize; ++i) {
+    std::cout << " " << int_list[i];
+  }
+  std::cout << "]";
+
+  return os;
+}
+
+} // namespace cookbook
