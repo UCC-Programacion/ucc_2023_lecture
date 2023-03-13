079c1dc47ee4d6fe6b1c677ab03539c342f3538a
Matias Volpe <kratico@users.noreply.github.com>
2023-03-13 10:03:32 -0300

    feat: add u1 ej04

--- u1/ej04/main.cpp
+++ u1/ej04/main.cpp
@@ -1,7 +1,30 @@
+#include <cassert>
 #include <iostream>
 
+int factorial(int);
+
 int main() {
-  std::cout << "main" << std::endl;
+  std::cout << "factorial(0) " << factorial(0) << std::endl;
+  std::cout << "factorial(3) " << factorial(3) << std::endl;
+  std::cout << "factorial(5) " << factorial(5) << std::endl;
 
   return 0;
+}
+
+// f(0) = 1
+// f(1) = 1  = 1 * f(0)
+// f(2) = 2  = 2 * f(1)
+// f(3) = 6  = 3 * f(2)
+// f(4) = 24 = 4 * f(3)
+// f(4) = 24 = 4 * 3 * 2 * 1
+// f(n) = ?  = n * f(n-1)
+
+int factorial(int n) {
+  assert(n >= 0);
+
+  if (n == 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 }
